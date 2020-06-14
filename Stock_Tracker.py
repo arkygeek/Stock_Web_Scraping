233e4a00870ddb3061d5fc964d5b4c0be5bbc13f
Jared Jorgenson <38658539+jared-jorgenson@users.noreply.github.com>
2020-06-14 17:26:52 +0100

    Update Stock_Tracker.py

--- Stock_Tracker.py
+++ Stock_Tracker.py
@@ -5,57 +5,37 @@ import pandas as pd
 
 def UPS_Tracker():
     URL = 'https://uk.finance.yahoo.com/quote/UPS?p=UPS&.tsrc=fin-srch'
-
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.text, 'lxml')
-
     price = soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-
     print(price)
 
 def Mircrosoft_Tracker():
     URL = 'https://uk.finance.yahoo.com/quote/MSFT?p=MSFT&.tsrc=fin-srch'
-
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.text, 'lxml')
-
     price = soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-
     print(price)
 
 def RY_Canada_Tracker():
     URL = 'https://uk.finance.yahoo.com/quote/RY?p=RY&.tsrc=fin-srch'
-
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.text, 'lxml')
-
     price = soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-
     print(price) 
 
 def Coca_Cola_Tracker():
     URL = 'https://uk.finance.yahoo.com/quote/KO?p=KO&.tsrc=fin-srch'
-
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.text, 'lxml')
-
     price = soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-
     print(price)
 
 def Nintendo_Tracker():
     URL = 'https://uk.finance.yahoo.com/quote/NTDOY?p=NTDOY&.tsrc=fin-srch'
-
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.text, 'lxml')
-
     price = soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-
     print(price)
 
 while True:
